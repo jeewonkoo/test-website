528322a3b4441228b3aa061adb68124f57c7c812
Jeewon Koo <jeewonk2@illinois.edu>
2022-06-21 10:28:23 +0900

    url redirect to mypads

--- static/index.js
+++ static/index.js
@@ -1,8 +1,3 @@
-
-function NewTabPublic() {
-    window.open("templates/etherpad-lite-service.html");
-}
-
-function NewTabLocal() {
-    window.open("http://127.0.0.1:9001/list");
+function openEtherpad() {
+    window.open("http://127.0.0.1:9001/mypads/?/login");
 }
