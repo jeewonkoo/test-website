1d191c70fc015d9b8a178cf5d813814274dcb505
Jeewon Koo <jeewonk2@illinois.edu>
2022-06-21 11:06:12 +0900

    add padlist button

--- static/index.js
+++ static/index.js
@@ -1,3 +1,7 @@
 function openEtherpad() {
-    window.open("http://127.0.0.1:9001/mypads/?/login");
+    window.open("http://127.0.0.1:9001/mypads");
+}
+
+function openPadlist() {
+    window.open("http://127.0.0.1:9001/list");
 }
